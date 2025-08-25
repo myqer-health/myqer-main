ad0e4b5ce54e69a7e2d895c6287ed9a1ad678586
myqer-health <hello.myqer@gmail.com>
2025-08-25 03:17:35 +0100

    Delete functions/index.ts

--- functions/index.ts
+++
@@ -1,4 +0,0 @@
-// qr-generate Edge Function (placeholder)
-import { serve } from "https://deno.land/std@0.168.0/http/server.ts";
-
-serve((req) => new Response(JSON.stringify({"function":"qr-generate","ok":true}), {"headers":{"content-type":"application/json"}}));
